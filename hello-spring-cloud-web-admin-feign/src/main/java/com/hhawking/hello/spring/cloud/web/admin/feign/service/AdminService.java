8b0630158be1db6cc69fbac34ee1e2ae02c21a13
HHawking <hhawking@aliyun.com>
2018-11-23 22:57:00 +0800

    学习

--- hello-spring-cloud-web-admin-feign/src/main/java/com/hhawking/hello/spring/cloud/web/admin/feign/service/AdminService.java
+++ hello-spring-cloud-web-admin-feign/src/main/java/com/hhawking/hello/spring/cloud/web/admin/feign/service/AdminService.java
@@ -1,10 +1,11 @@
 package com.hhawking.hello.spring.cloud.web.admin.feign.service;
 
+import com.hhawking.hello.spring.cloud.web.admin.feign.service.hystrix.AdminServiceHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "hello-spring-cloud-admin")
+@FeignClient(value = "hello-spring-cloud-admin",fallback = AdminServiceHystrix.class)
 public interface AdminService {
 
     @GetMapping(value = "test")
--- hello-spring-cloud-web-admin-ribbon/src/main/java/com/hhawking/hello/spring/cloud/web/admin/ribbon/controller/TestController.java
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/hhawking/hello/spring/cloud/web/admin/ribbon/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.hhawking.hello.spring.cloud.web.admin.ribbon.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,12 @@ public class TestController {
     RestTemplate restTemplate;
 
     @GetMapping("test")
+    @HystrixCommand(fallbackMethod = "testError")
     public String test(String msg){
         return restTemplate.getForObject("HTTP://hello-spring-cloud-admin/test?msg="+msg,String.class);
+    }
+
+    public String testError(String msg){
+        return "Hi，your message is :\"" + msg + "\" but request error.";
     }
 }
